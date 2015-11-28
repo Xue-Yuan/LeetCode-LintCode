61cd4d53224e594313525cd917ce1c7b5a0dfaf4
Yuan Xue <cqnkxy@gmail.com>
2015-11-27 22:38:18 -0800

    dessert before night

---
+++ Valid Palindrome.cpp
@@ -0,0 +1,35 @@
+/*
+    Given a string, determine if it is a palindrome, considering 
+        only alphanumeric characters and ignoring cases.
+
+    For example,
+        "A man, a plan, a canal: Panama" is a palindrome.
+        "race a car" is not a palindrome.
+
+    Note:
+        Have you consider that the string might be empty? This is 
+            a good question to ask during an interview.
+
+    For the purpose of this problem, we define empty string as 
+        valid palindrome.
+*/
+
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int beg = 0, end = (int)s.size() - 1;
+        while(beg < end)
+        {
+            while(beg < end && !(isalpha(s[beg]) || isdigit(s[beg])))
+                beg++;
+            while(beg < end && !(isalpha(s[end]) || isdigit(s[end])))
+                end--;
+            if(beg < end)
+                if(tolower(s[beg++]) != tolower(s[end--]))
+                    return false;
+        }
+        return true;
+    }
+};
